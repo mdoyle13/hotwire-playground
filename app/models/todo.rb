296908f9c42e2a4c3e7f2c05b1fcdc8cd2adbd03
mike doyle <mdoyle13@gmail.com>
2022-02-21 16:36:36 -0600

    upgrade and cleanup

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -4,7 +4,7 @@ class Todo < ApplicationRecord
   scope :complete, -> { where(completed: true) }
   scope :incomplete, -> { where(completed: [false, nil]) }
 
-  # after_create_commit -> { broadcast_prepend_to "todos" }
-  # after_update_commit -> { broadcast_replace_to "todos" }
-  # after_destroy_commit -> { broadcast_remove_to "todos" }
+  after_create_commit -> { broadcast_prepend_to "todos" }
+  after_update_commit -> { broadcast_replace_to "todos" }
+  after_destroy_commit -> { broadcast_remove_to "todos" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       get :complete, :incomplete
     end
     member do
-      post :toggle
+      patch :toggle
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
